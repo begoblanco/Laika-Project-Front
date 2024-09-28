e016ce914e0ca9c7249889dde36cbaf23c8d4775
begoblanco <begoblanfer@gmail.com>
2024-09-28 11:00:36 +0200

    router configuration with permits

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/authStore';
 import HomeView from '../views/HomeView.vue'
 import Login from '@/components/auth/Login.vue'
 import SignUp from '@/components/auth/SignUp.vue'
@@ -8,37 +9,51 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'Login',
+      name: 'login',
       component: Login,
       meta: { layout: 'empty', requiresAuth: false },
     },
     {
       path: '/register',
-      name: 'SignUp',
+      name: 'signUp',
       component: SignUp,
       meta: { layout: 'empty', requiresAuth: false },
     },
     {
       path: '/home',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/calendar',
       name: 'calendar',
-      component: () => import('../views/homeviews/CalendarView.vue')
+      component: () => import('../views/homeviews/CalendarView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/notes',
       name: 'notes',
-      component: () => import('../views/homeviews/NotesView.vue')
+      component: () => import('../views/homeviews/NotesView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/chat',
       name: 'chat',
-      component: () => import('../views/homeviews/ChatView.vue')
+      component: () => import('../views/homeviews/ChatView.vue'),
+      meta: { requiresAuth: true },
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+  if (to.meta.requiresAuth && ((!authStore.token) || authStore.isExpired())) {
+    authStore.logout();
+    next(router.resolve({ "name": "login" }).fullPath);
+  } else {
+    next();
+  }
+});
 
 export default router
